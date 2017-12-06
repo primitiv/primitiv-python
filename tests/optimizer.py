f7a7ae66feb3327fd512affc7380e27bb497ffaa
Koichi Akabe <vbkaisetsu@gmail.com>
2017-12-06 15:37:53 +0900

    Test modification of parameters that are added to an optimizer

--- tests/optimizer.py
+++ tests/optimizer.py
@@ -4,12 +4,16 @@ from primitiv import initializers as I
 from primitiv import Model
 from primitiv import optimizers as O
 from primitiv import Parameter
+from primitiv import tensor_operators as tF
 
 import unittest
 
 
 class TestModel(Model):
-    pass
+    def __init__(self):
+        self.param = Parameter([1], I.Constant(0))
+        self.param.gradient = tF.raw_input([1], [1])
+        self.scan_attributes()
 
 
 class Optimizer(unittest.TestCase):
@@ -31,9 +35,15 @@ class Optimizer(unittest.TestCase):
 
     def test_optimizer_add(self):
         model = TestModel()
-        p = Parameter([5], I.Constant(0))
+        p = Parameter([1], I.Constant(0))
+        p.gradient = tF.raw_input([1], [1])
         optimizer = O.Adam()
         optimizer.set_weight_decay(1e-6)
         optimizer.set_gradient_clipping(5)
         optimizer.add(model)
         optimizer.add(p)
+        self.assertEqual(p.gradient.to_list()[0], 1)
+        self.assertEqual(model.param.gradient.to_list()[0], 1)
+        optimizer.reset_gradients()
+        self.assertEqual(p.gradient.to_list()[0], 0)
+        self.assertEqual(model.param.gradient.to_list()[0], 0)
