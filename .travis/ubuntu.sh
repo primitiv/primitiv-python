dff68cf2b44126fea67c693d1b693a80ecb2b45f
Koichi Akabe <vbkaisetsu@gmail.com>
2018-07-18 11:53:04 +0900

    Update primitiv-core

--- .travis/ubuntu.sh
+++ .travis/ubuntu.sh
@@ -7,11 +7,15 @@ docker run --name travis-ci -v $TRAVIS_BUILD_DIR:/primitiv-python -td ubuntu:rol
 
 # install
 docker exec travis-ci bash -c "apt update"
-docker exec travis-ci bash -c "apt install -y git build-essential cmake python3-dev python3-pip python3-numpy libeigen3-dev"
+docker exec travis-ci bash -c "apt install -y build-essential cmake python3-dev python3-pip python3-numpy libeigen3-dev"
 docker exec travis-ci bash -c "pip3 install cython scikit-build"
 
 # install OpenCL environment
-docker exec travis-ci bash -c "apt install -y opencl-headers libclblas-dev pkg-config libhwloc-dev libltdl-dev ocl-icd-dev ocl-icd-opencl-dev clang llvm-dev libclang-dev libz-dev"
+docker exec travis-ci bash -c "apt install -y opencl-headers git wget pkg-config libhwloc-dev libltdl-dev ocl-icd-dev ocl-icd-opencl-dev clang llvm-dev libclang-dev libz-dev"
+docker exec travis-ci bash -c "wget https://github.com/CNugteren/CLBlast/archive/1.2.0.tar.gz -O ./clblast.tar.gz"
+docker exec travis-ci bash -c "mkdir ./clblast"
+docker exec travis-ci bash -c "tar xf ./clblast.tar.gz -C ./clblast --strip-components 1"
+docker exec travis-ci bash -c "cd ./clblast && cmake . && make && make install"
 # pocl 0.13 does not contain mem_fence() function that is used by primitiv.
 # We build the latest pocl instead of using distribution's package.
 # See: https://github.com/pocl/pocl/issues/294
